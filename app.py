f98c0a5b7950c7eacb0d9013ce4b9268fde67ccc
Haram Park <harampark@kakao.com>
2021-11-10 15:20:36 +0900

    upload html

--- app.py
+++ app.py
@@ -27,22 +27,3 @@ DATA_URL = (
 def load_data():
     data = pd.read_csv(DATA_URL, encoding="utf-8", index_col=0)
     return data
-
-
-data = load_data()
-
-st.write(data)
-
-# Make layer
-layer = pdk.Layer(
-    "PolygonLayer",  # 사용할 Layer 타입
-    data,  # 시각화에 쓰일 데이터프레임
-    get_polygon="coordinates",  # geometry 정보를 담고있는 컬럼 이름
-    get_fill_color="[0, 255*정규화인구, 0]",  # 각 데이터 별 rgb 또는 rgba 값 (0~255)
-    pickable=True,  # 지도와 interactive 한 동작 on
-    auto_highlight=True,  # 마우스 오버(hover) 시 박스 출력
-)
-
-# Set the viewport location
-center = [129.05562775, 35.1379222]
-view_state = pdk.ViewState(longitude=center[0], latitude=center[1], zoom=10)
